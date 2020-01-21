3e78b49d69df98bb4b3d8920a23ed4a77f36ddcf
Melvin <kartik3163@gmail.com>
2020-01-21 23:51:26 +0530

    Adapt Blob Extraction Script for RMX1801
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -17,19 +17,18 @@
 
 set -e
 
-DEVICE=X01BD
-VENDOR=asus
+DEVICE=RMX1801
+VENDOR=oppo
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-ARROW_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$ARROW_ROOT"/vendor/arrow/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
-    exit 1
 fi
 . "$HELPER"
 
@@ -55,25 +54,8 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$ARROW_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
-#extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
 
-BLOB_ROOT="$ARROW_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-
-TWRP_QSEECOMD="$BLOB_ROOT"/recovery/root/sbin/qseecomd	
-TWRP_GATEKEEPER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.gatekeeper@1.0-service-qti	
-TWRP_KEYMASTER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.keymaster@3.0-service-qti	
-
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_QSEECOMD"	
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_GATEKEEPER"	
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_KEYMASTER"	
-
-IMSCMSERVICE="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice.xml	
-IMSCMSERVICE_1_1="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice_1_1.xml	
-
-sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE"	
-sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE_1_1
-
-. "$MY_DIR"/setup-makefiles.sh
+"$MY_DIR"/setup-makefiles.sh
